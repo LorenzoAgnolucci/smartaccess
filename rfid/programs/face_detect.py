adeded826078e05ecbdff7038646df071d926cdf
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2019-08-14 12:10:39 +0200

    Implemented face_crop function

--- rfid/programs/face_detect.py
+++ rfid/programs/face_detect.py
@@ -1,5 +1,5 @@
 from ibm_watson import VisualRecognitionV3
-import PIL
+from PIL import Image
 from picamera import PiCamera
 
 
@@ -12,9 +12,13 @@ def face_crop(id, face_location):
     # TODO implement with PILLOW
     #  the method should save a fixed dimension squared image in the dir /smartaccess/media/face_crops
     #  named <id>.jpg where id is the id log passed through get_photo_data()
-    camera = PiCamera()
-    camera.capture("{}.jpg".format(id))
-    camera.close()
+    left = face_location['left']
+    top = face_location['top']
+    right = left + face_location['width']
+    bottom = top + face_location['height']
+    img = Image.open('smartaccess/media/tmp.jpg')
+    img = img.crop((left, top, right, bottom)).resize((300, 300), Image.ANTIALIAS)
+    img.save('smartaccess/media/face_crops/{}.jpg'.format(id), "JPEG")
 
 
 def get_photo_data(id):
