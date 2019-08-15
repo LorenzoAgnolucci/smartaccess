c5bc29762effc163ddca8956559ae9d413d73925
Lorenzo Agnolucci <lory.agnolucci@gmail.com>
2019-08-15 20:26:47 +0200

    Changed behaviour of form and label of remaining_accesses according to it. Added help_texts.

--- rfid/forms.py
+++ rfid/forms.py
@@ -7,13 +7,6 @@ from .models import RFIDCard
 
 class WriteCardForm(forms.ModelForm):
 
-    # # FIXME clean method works. Try again with clean_card_id and find the best way to show the error
-    # def clean_card_id(self):
-    #     card_id = self.cleaned_data['card_id']
-    #     if RFIDCard.objects.filter(pk=card_id) == []:
-    #         raise forms.ValidationError('Card not registered, please add it to the database first')
-    #     return card_id
-
     def clean(self):
         # To keep the main validation and error messages
         cleaned_data = super(WriteCardForm, self).clean()
@@ -22,11 +15,11 @@ class WriteCardForm(forms.ModelForm):
         if not RFIDCard.objects.filter(pk=card_id).exists():
             self.add_error('card_id', "Card not registered, please add it to the database first")
 
-        if cleaned_data['remaining_accesses'] <= 0:
-            self.add_error('remaining_accesses', "Value must be greater than 0")
+        if cleaned_data['remaining_accesses'] < 0:
+            self.add_error('remaining_accesses', "Value must be greater or equal than 0")
 
-        if cleaned_data['expiration_date'] <= datetime.date.today():
-            self.add_error('expiration_date', "Date must be in the future")
+        if cleaned_data['expiration_date'] < datetime.date.today():
+            self.add_error('expiration_date', "Date can't be in the past")
 
         return cleaned_data
 
@@ -36,6 +29,13 @@ class WriteCardForm(forms.ModelForm):
         widgets = {
             "card_id": forms.TextInput(attrs={'readonly': True, 'blank': True}),
             "expiration_date": forms.DateInput(attrs={'class': 'datepicker', 'id': 'datepicker'})
+        }
+        help_texts = {
+            "remaining_accesses": "0 won't change the number of remaining accesses",
+            "expiration_date" : "Today's date won't change the expiration date"
+        }
+        labels = {
+            "remaining_accesses": "Accesses to add"
         }
 
 # TODO change behaviour of form: from 'new remaining accesses number' to 'accesses increment'
