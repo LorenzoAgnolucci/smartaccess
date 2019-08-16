cc55950d11b87cb1823f6847a8c31e59616c692e
Vittorio <zampinetti@gmail.com>
2019-08-16 18:37:13 +0200

    Added accounts app for login and home page template

--- smartaccess/urls.py
+++ smartaccess/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls, name='admin'),
     path('rfid/', include('rfid.urls')),
+    path('accounts/', include('django.contrib.auth.urls')),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+
 ]
